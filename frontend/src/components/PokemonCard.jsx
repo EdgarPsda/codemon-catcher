5be526cf2c792afa7afa4321c232befa25678eba
EdgarPsda <edgar.psda@gmail.com>
2024-02-02 14:40:49 -0800

    Pagination
    Added pagination functionality and buttons styles.

--- frontend/src/components/PokemonCard.jsx
+++ frontend/src/components/PokemonCard.jsx
@@ -5,13 +5,16 @@ const PokemonCard = ({ pokemon }) => {
             <div className="relative h-80 w-full overflow-hidden rounded-lg bg-white sm:aspect-h-1 sm:aspect-w-2 lg:aspect-h-1 lg:aspect-w-1 group-hover:opacity-75 sm:h-64">
                 <img src={pokemon.sprite} alt="pokemon sprite" className="h-full w-full object-cover object-center" />
             </div>
-            <h3 className="mt-6 text-sm text-white">
-                <a href="#">
-                    <span className="absolute inset-0"></span>
-                    {pokemon.name}
-                </a>
-            </h3>
-            <p className="text-base font-semibold text-white">#{pokemon.pokeId}</p>
+            <div className="flex justify-between mt-3 mb-8">
+                <h3 className="text-sm text-white">
+                    <a href="#">
+                        <span className="absolute inset-0"></span>
+                        {pokemon.name.toUpperCase()}
+                    </a>
+                </h3>
+                <p className="text-base font-semibold text-white">#{pokemon.pokeId}</p>
+
+            </div>
         </div>
     )
 }
--- frontend/src/components/PokemonList.jsx
+++ frontend/src/components/PokemonList.jsx
@@ -4,10 +4,12 @@ import PokemonCard from "./PokemonCard"
 const PokemonList = () => {
 
     const [pokemons, setPokemons] = useState([]);
+    const [url, setUrl] = useState("http://localhost:4000/api/pokemons");
+    const [prev, setPrev] = useState("");
+    const [next, setNext] = useState("");
 
     useEffect(() => {
         const apiRequest = async () => {
-            const url = "http://localhost:4000/api/pokemons";
             const res = await fetch(url);
             const result = await res.json();
 
@@ -22,17 +24,87 @@ const PokemonList = () => {
             });
 
             setPokemons(pokemonsArray);
+            setPrev(result.previous);
+            setNext(result.next);
         }
 
         apiRequest();
-    }, []);
+    }, [url]);
+
+    const handlePrev = () => {
+        setUrl(prev);
+    }
+
+    const handleNext = () => {
+        setUrl(next);
+    }
+
+
 
     return (
         <>
             <div className="bg-gray-600 rounded-xl mx-3">
                 <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-32">
-                        <h2 className="text-2xl font-bold text-white">Pokedex</h2>
+                    <div className="mx-auto max-w-2xl py-16 sm:py-24 lg:max-w-none lg:py-10">
+                        <h2 className="text-5xl font-bold text-white">Pokedex</h2>
+                        <div>
+                            <div className="flex justify-between mt-5">
+                                <button
+                                    type="button"
+                                    className="
+                                        text-white 
+                                        bg-gradient-to-r 
+                                        from-teal-400 
+                                        via-teal-500 
+                                        to-teal-600 
+                                        hover:bg-gradient-to-br 
+                                        focus:ring-4 
+                                        focus:outline-none 
+                                        focus:ring-teal-300 
+                                        dark:focus:ring-teal-800 
+                                        font-medium 
+                                        rounded-lg 
+                                        text-sm 
+                                        px-5 
+                                        py-2.5 
+                                        text-center 
+                                        me-2 
+                                        mb-2
+                                        uppercase"
+                                    onClick={handlePrev}
+                                    disabled={prev ? false : true}
+                                >
+                                    Previous
+                                </button>
+                                <button
+                                    type="button"
+                                    className="
+                                        text-white
+                                        bg-gradient-to-r 
+                                        from-teal-400 
+                                        via-teal-500 
+                                        to-teal-600
+                                        hover:bg-gradient-to-br 
+                                        focus:ring-4
+                                        focus:outline-none
+                                        focus:ring-teal-300
+                                        dark:focus:ring-teal-800
+                                        font-medium
+                                        rounded-lg
+                                        text-sm
+                                        px-5
+                                        py-2.5
+                                        text-center
+                                        me-2
+                                        mb-2
+                                        uppercase"
+                                    onClick={handleNext}
+                                    disabled={next ? false : true}
+                                >
+                                    Next
+                                </button>
+                            </div>
+                        </div>
                         <div className="mt-6 space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
                             {
                                 pokemons.map(pokemon => (
@@ -40,11 +112,11 @@ const PokemonList = () => {
                                         pokemon={pokemon}
                                         key={pokemon.pokeId}
                                     >
-
                                     </PokemonCard>
                                 )
                                 )
                             }
+
                         </div>
                     </div>
                 </div>
